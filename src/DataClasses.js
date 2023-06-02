e28a2ada19210428aa691c926fb00e19465efdab
Mark van Dijk <markvandijk@happyhorizon.com>
2023-06-02 08:07:05 +0200

    Update Data Classes to version 1.2.0.

--- src/DataClasses.js
+++ src/DataClasses.js
@@ -1,4 +1,4 @@
-//version = "V 1.1.0" Add UNRELEASED if the current version is not yet published to the CDN. When releasing remove UNRELEASED.
+//version = "V 1.2.0" Add UNRELEASED if the current version is not yet published to the CDN. When releasing remove UNRELEASED.
 
 window.InputType = {
     Text: "text",
